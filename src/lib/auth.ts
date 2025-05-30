2fe28ed214d900ab3151d73331e61b45e1128b3b
Printer <sriapirath1.1@gmail.com>
2025-05-30 10:38:36 +0700

    update schema

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,18 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import db from "@/db"; // your drizzle instance
+import { account, session, user, verification } from "@/db/schema";
  
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: "mysql", 
-    })
+        schema: {user, account, session, verification},
+        usePlural: false //ไม่ต้องเติม s ข้างหลัง
+    }),
+    emailAndPassword: {
+        enabled: true,
+        autoSignIn: false,
+        requireEmailVerification: false,
+        minPasswordLength: 6
+    }
 });
